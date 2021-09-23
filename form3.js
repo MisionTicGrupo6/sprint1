299b678be1b48ba745092b852dfe6ca5c4b5bacf
MisionTicGrupo6 <90813178+MisionTicGrupo6@users.noreply.github.com>
2021-09-23 16:42:46 -0500

    Se crea Documento base  Sprint2 (Marly Johana Yepes)
    Se crea documento base, para entrega del sprint2

---
+++ form3.js
@@ -0,0 +1,9 @@
+funtion vaidar_telefono(string){
+
+}
+funtion validar_direccion(string){
+
+}
+
+module.exports.validar_telefono=validar_telefono;
+module.exports.validar_direccion=validar_direccion;
